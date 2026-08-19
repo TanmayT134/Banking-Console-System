fda89b0a19694e5fdb8f2bcfa9544ae8f3fe8e8b
TanmayT134 <tawade.tanmay134@gmail.com>
2026-08-19 11:11:37 +0530

    Cleaning Code

--- compile.bat
+++ compile.bat
@@ -2,6 +2,6 @@
 setlocal
 if not exist out mkdir out
 set "CP=lib\javax.mail-1.6.2.jar;lib\activation-1.1.1.jar"
-javac -cp "%CP%" -d out src\com\tanmay\corebanking\Main.java src\com\tanmay\corebanking\enums\*.java src\com\tanmay\corebanking\exception\*.java src\com\tanmay\corebanking\model\*.java src\com\tanmay\corebanking\repository\*.java src\com\tanmay\corebanking\service\*.java src\com\tanmay\corebanking\ui\*.java src\com\tanmay\corebanking\util\*.java src\com\tanmay\corebanking\test\*.java
+javac -cp "%CP%" -d out src\com\tanmay\corebanking\Main.java src\com\tanmay\corebanking\enums\*.java src\com\tanmay\corebanking\exception\*.java src\com\tanmay\corebanking\model\*.java src\com\tanmay\corebanking\repository\*.java src\com\tanmay\corebanking\service\*.java src\com\tanmay\corebanking\ui\*.java src\com\tanmay\corebanking\util\*.java
 if errorlevel 1 exit /b 1
 echo Compilation successful.
